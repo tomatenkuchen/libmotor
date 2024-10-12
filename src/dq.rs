a0c035d9771fbea2900b1cfb15811102170fb9a5
tomatenkuchen <t.w92@gmx.de>
2024-10-12 20:18:40 +0200

    some progress on dq trafo

---
+++ src/dq.rs
@@ -0,0 +1,13 @@
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+
+//! [park](https://en.wikipedia.org/wiki/Direct-quadrature-zero_transformation)
+//! and
+//! [clarke](https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_transformation)
+//! transformations and inverses
+
+pub mod dq{
+    pub fn ab2dq(ab: num::complex::Complex, angle f32)->num::complex::Complex{
+        ab.exp
+    }
+}

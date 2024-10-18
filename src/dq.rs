9afd8191db8e71aae103d97f824ae8e6563ca6b0
tomatenkuchen <t.w92@gmx.de>
2024-10-18 23:43:10 +0200

    added ab2dq

--- src/dq.rs
+++ src/dq.rs
@@ -5,7 +5,16 @@
 //! and
 //! [clarke](https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_transformation)
 //! transformations and inverses
+//!
 
-pub fn ab2dq(ab: num::complex::Complex<f32>, angle: f32) -> num::complex::Complex<f32> {
-    ab * num::complex::Complex::
+use num::{complex::c32, Complex};
+
+/// park inverse is nothing more then a rotation. since rotations aren't a nostd feature, we're
+/// doing it the hard way
+pub fn ab2dq(ab: Complex<f32>, angle: f32) -> Complex<f32> {
+    let sin_angle = angle.sin();
+    let cos_angle = angle.cos();
+    let real = cos_angle * ab.re + sin_angle * ab.im;
+    let imag = cos_angle * ab.im - sin_angle * ab.re;
+    c32(real, imag)
 }
